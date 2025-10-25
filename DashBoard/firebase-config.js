cb6f846cc5f75ff450e8eb49941bc9eb5387cd57
Aditya Sharma <adityaforwork09@gmail.com>
2025-10-26 04:08:50 +0530

    Create firebase-config.js

---
+++ DashBoard/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCc8PCc1IYikEQXJ4_5avlOJM9JqIrH7Ac",
+  authDomain: "backbeacon.firebaseapp.com",
+  projectId: "backbeacon",
+  storageBucket: "backbeacon.firebasestorage.app",
+  messagingSenderId: "287132718497",
+  appId: "1:287132718497:web:99e72324a1ca257f1627e0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
