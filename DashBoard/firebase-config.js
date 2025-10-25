56fbd29ff7875103c068f82c8d1879ff856e3b5a
Aditya Sharma <adityaforwork09@gmail.com>
2025-10-26 04:16:20 +0530

    Export firebaseConfig for external use

--- DashBoard/firebase-config.js
+++ DashBoard/firebase-config.js
@@ -1,10 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCc8PCc1IYikEQXJ4_5avlOJM9JqIrH7Ac",
   authDomain: "backbeacon.firebaseapp.com",
   projectId: "backbeacon",
@@ -12,6 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "287132718497",
   appId: "1:287132718497:web:99e72324a1ca257f1627e0"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
